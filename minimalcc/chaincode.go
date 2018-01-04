2d242e4948b750b2b06deeaa6c29e49ae7a311d2
Paul Sitoh <paulwizviz@gmail.com>
2018-01-04 12:02:59 +0000

    Change to using logger instead of fmt
    modified:   minimalcc/chaincode.go

--- minimalcc/chaincode.go
+++ minimalcc/chaincode.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
+
+var logger = shim.NewLogger("minimalcc")
 
 // SimpleChaincode representing a class of chaincode
 type SimpleChaincode struct{}
 
 // Init to initiate the SimpleChaincode class
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("Hello Init")
+	logger.Info("Hello Init")
 	return shim.Success([]byte("Init called"))
 }
 
 // Invoke a method specified in the SimpleChaincode class
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("Hello Invoke")
+	logger.Info("Hello Invoke")
 	return shim.Success([]byte("Invoke"))
 }
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
-		fmt.Printf("Error: %s", err)
+		logger.Debugf("Error: %s", err)
 	}
 }
